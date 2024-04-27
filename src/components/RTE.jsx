44bcce5073df4e5017cdb5a93ede364fc763c452
dhanji-kumar <kdhanji183@gmail.com>
2024-04-27 11:43:54 +0530

    add:improve seo,tinymce id fix

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -14,7 +14,7 @@ export default function RTE({ name, control, label, defaultValue = "" }) {
         control={control}
         render={({ field: { onChange } }) => (
           <Editor
-            apiKey={conf.tinyMce}
+            apiKey={conf.tinyMceId}
             initialValue={defaultValue}
             init={{
               initialValue: defaultValue,
--- src/conf/conf.js
+++ src/conf/conf.js
@@ -4,7 +4,7 @@ const conf = {
   appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
   appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
   appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
-  tinyMce: String(import.meta.env.VITE_TINYMCE_KEY),
+  tinyMceId: String(import.meta.env.VITE_TINYMCE_ID),
 };
 // there was a name issue with the import.meta.env.VITE_APPWRITE_URL, it was later fixed in debugging video
 
