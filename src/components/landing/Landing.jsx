0d6eb43df7e8188906de63f45de0abb1676e6bdb
Dhanji Kumar <kdhanji183@gmail.com>
2024-05-10 16:24:00 +0530

    initial for landing pages

--- src/components/landing/Landing.jsx
+++ src/components/landing/Landing.jsx
@@ -3,20 +3,6 @@ import { Link } from "react-router-dom";
 
 export default function Landing({input='Explore blog',link='/login'}) {
     return (
-        <div className="bg-[#DDE6ED] dark:bg-[#27374D] min-h-screen flex flex-col justify-center items-center">
-            <div className="max-w-3xl mx-auto text-center">
-                <h1 className="text-4xl md:text-6xl font-bold dark:text-[#d8d5d5] mb-6 text-stone-900">
-                    Welcome to chaiXblog
-                </h1>
-                <p className="text-lg md:text-xl dark:text-[#bcbcbc] text-stone-700 mb-8">
-                    Explore the latest articles, insights, and stories from our talented writers.
-                </p>
-                <Link to={link}
-                    className="bg-[#526D82] hover:bg-[#243e63] text-[#c1d9eb] font-semibold py-3 px-8 rounded-full shadow-lg transition duration-300 ease-in-out border"
-                >
-                    {input}
-                </Link>
-            </div>
-        </div>
+        
     )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,58 +1,42 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from "react";
 import appwriteService from "../appwrite/config";
-import { Landing, useDynamicTitle } from '../components'
-import { useSelector } from 'react-redux';
+import { Landing, useDynamicTitle } from "../components";
+import { useSelector } from "react-redux";
 
 function Home() {
-    useDynamicTitle()
-    const [loding, setLoading] = useState(false)
-    const user = useSelector(state => state.auth.status)
-    const [posts, setPosts] = useState([])
+  useDynamicTitle();
+  const [loding, setLoading] = useState(false);
+  const user = useSelector((state) => state.auth.status);
+  const [posts, setPosts] = useState([]);
 
-    useEffect(() => {
-        setLoading(true)
-        appwriteService.getPosts().then((posts) => {
-            if (posts) {
-                setPosts(posts.documents)
-                setLoading(false)
-            }
-        })
-    }, [])
+  useEffect(() => {
+    setLoading(true);
+    appwriteService.getPosts().then((posts) => {
+      if (posts) {
+        setPosts(posts.documents);
+        setLoading(false);
+      }
+    });
+  }, []);
 
+  if (!user) {
+    return <Landing input="create account" link="/signup" />;
+  }
 
-    if (!user) {
-        return (
-            <Landing
-                input='create account'
-                link='/signup'
-            />
-        );
-    }
+  if (loding) {
+    return <Landing input="loading..." link="#" />;
+  }
 
-
-    if (loding) {
-        return (
-            <Landing
-                input='loading...'
-                link='#'
-            />
-        )
-    }
-
-    return (
-        user === true ? (
-            !loding && (
-                <Landing
-                    input={posts.length === 0 ? "add-posts" : "read-post"}
-                    link={posts.length === 0 ? "/add-post" : "/all-posts"}
-                />)
-        ) : (
-            <Landing
-                input='create account'
-                link='/signup'
-            />
-        )
+  return user === true ? (
+    !loding && (
+      <Landing
+        input={posts.length === 0 ? "add-posts" : "read-post"}
+        link={posts.length === 0 ? "/add-post" : "/all-posts"}
+      />
     )
+  ) : (
+    <Landing input="create account" link="/signup" />
+  );
 }
 
-export default Home
+export default Home;
